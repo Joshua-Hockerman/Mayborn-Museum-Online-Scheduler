78a399be1b9c0d90e4b8645e9095fb35375bfdb5
Joshua Hockerman <jhockerman96@gmail.com>
2022-09-23 22:49:22 -0500

    Added initial home page.

---
+++ MaybornApp/urls.py
@@ -0,0 +1,11 @@
+"""Defines the URL patterns for the Mayborn App"""
+
+from django.urls import path
+
+from . import views
+
+app_name = "MaybornApp"
+urlpatterns = [
+    # Home page
+    path("", views.index, name="index"),
+]
--- MaybornApp/views.py
+++ MaybornApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """The home page for learning logs."""
+    return render(request, "MaybornApp/index.html")
