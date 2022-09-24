750d78e39419fcd91893ba2c14b36ec088ca12be
Joshua Hockerman <jhockerman96@gmail.com>
2022-09-23 22:22:41 -0500

    Adding more models and initial DB test data.

--- MaybornApp/admin.py
+++ MaybornApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Topic
+from .models import Topic, Schedule
 
 admin.site.register(Topic)
+admin.site.register(Schedule)
--- MaybornApp/models.py
+++ MaybornApp/models.py
@@ -12,3 +12,34 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string representation of the model."""
         return self.text
+
+
+OPTION_CHOICES = (
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5),
+    (6, 6),
+    (7, 7),
+    (8, 8),
+    (9, 9),
+    (10, 10),
+)
+
+
+class Schedule(models.Model):
+    """A place for the employee to enter their holiday preference information"""
+
+    Holiday_1 = models.SmallIntegerField()
+    Holiday_2 = models.SmallIntegerField()
+    Holiday_3 = models.SmallIntegerField()
+    Holiday_4 = models.SmallIntegerField()
+    Holiday_5 = models.SmallIntegerField()
+    Holiday_6 = models.SmallIntegerField()
+    Holiday_7 = models.SmallIntegerField()
+    Holiday_8 = models.SmallIntegerField()
+    Holiday_9 = models.SmallIntegerField()
+    Holiday_10 = models.SmallIntegerField()
+
+    Comments = models.CharField(max_length=500)
